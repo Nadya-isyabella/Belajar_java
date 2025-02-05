efc9f3011e1ca22e686b66d01da64a7c48770af5
Nadya-isyabella <nadyaisyabella@gmail.com>
2025-02-05 14:51:26 +0700

    Add files via upload

---
+++ Latihan5.java
@@ -0,0 +1,10 @@
+package com;
+import javax.swing.JOptionPane;
+public class Latihan5 {
+    public static void main(String[] args) {
+        String nama = "";
+        nama = JOptionPane.showInputDialog("Silahkan masukkan nama Anda");
+        String msg = "Hello " + nama + "!";
+        JOptionPane.showMessageDialog(null, msg);
+    }
+}
